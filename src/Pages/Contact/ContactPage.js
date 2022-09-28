a792d9b21cf0321c6cbf9e1b483472c197096619
mMarante <mg.marante@gmail.com>
2022-09-28 19:12:29 -0300

    Item Collection II

--- src/Pages/Contact/ContactPage.js
+++ src/Pages/Contact/ContactPage.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React,{useContext, useEffect} from 'react'
+import {db} from "../../utils/firebase";
+import {doc,getDoc, collection, getDocs, query, where, limit } from "firebase/firestore"
+
 
 const ContactPage = () => {
+
+  useEffect(()=>{
+
+    const getData = async()=>{
+      const queryRef = query(collection(db,"products"),where("category","==","Tortas"), limit(1));
+      const response = await getDocs(queryRef);
+      const productos = response.docs.map(doc => { 
+        const newProduct = {
+          ...doc.data(),
+          id: doc.id
+        }
+        return newProduct
+      })
+    }
+    getData();
+  },[])
   return (
     <div>ContactPage</div>
   )
